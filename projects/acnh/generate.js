89805ae7f0301011c3bc0f852aa17dbb29b71dec
Astronomy487 <astronomy487@gmail.com>
2021-01-04 22:40:57 -0600

    acnh but with objects
    i look like such a clown right now. but thats ok because now they all use objects instead of stupid extra indeces :)

--- projects/acnh/generate.js
+++ projects/acnh/generate.js
@@ -26,20 +26,20 @@ for (let i = 0; i < source.length; i++) {
   
   let monthmatch;
   if (!south) {
-    monthmatch = source[i][3][now.getMonth()];
+    monthmatch = source[i].annual[now.getMonth()];
   } else {
-    monthmatch = source[i][3][(now.getMonth()+6)%12];
+    monthmatch = source[i].annual[(now.getMonth()+6)%12];
   }
   
   let nextavailable;
-  if (monthmatch && source[i][4][now.getHours()]) {
+  if (monthmatch && source[i].daily[now.getHours()]) {
     newrow = '<tr class="available">';
     nextavailable = "now";
   } else if (monthmatch) {
     newrow = '<tr class="almostavailable">';
     findNextAvailable:
     for (a = now.getHours(); a < 30; a = (a+1)%24) {
-      if (source[i][4][a]) {
+      if (source[i].daily[a]) {
         nextavailable = hours[a];
         break findNextAvailable;
       }
@@ -48,7 +48,7 @@ for (let i = 0; i < source.length; i++) {
     newrow = '<tr class="unavailable">'
     findNextAvailable:
     for (a = now.getMonth(); a < 30; a = (a+1)%12) {
-      if (source[i][3][a]) {
+      if (source[i].annual[a]) {
         //next available month is found (a)
         if (!south) {
           nextavailable = months[a];
@@ -60,10 +60,10 @@ for (let i = 0; i < source.length; i++) {
     }
   }
   
-  newrow += "<td>"+source[i][0]+"</td>"; //name
-  newrow += "<td>"+source[i][1]+"</td>"; //location
-  if (source[i][5] != "bug") newrow += "<td>"+source[i][5]+"</td>"; //size, if applicable
-  newrow += "<td>"+commas(source[i][2])+" bells</td>"; //price
+  newrow += "<td>"+source[i].name+"</td>"; //name
+  newrow += "<td>"+source[i].found+"</td>"; //location
+  if (source[i][5] != "bug") newrow += "<td>"+source[i].size+"</td>"; //size, if applicable
+  newrow += "<td>"+commas(source[i].bells)+" bells</td>"; //price
   
   if (shownext) {
     newrow += "<td>"+nextavailable+"</td>";
@@ -72,9 +72,9 @@ for (let i = 0; i < source.length; i++) {
     for (let j = 0; j < 12; j++) {
       let availablethismonth;
       if (!south) {
-        availablethismonth = source[i][3][j];
+        availablethismonth = source[i].annual[j];
       } else {
-        availablethismonth = source[i][3][(j+6)%12];
+        availablethismonth = source[i].annual[(j+6)%12];
       }
       if (availablethismonth) {
         newrow += '<td class="yes">'+monthsshort[j]+'</td>';
@@ -86,7 +86,7 @@ for (let i = 0; i < source.length; i++) {
     
     newrow += '<td class="timescell"><table class="times" id=""><tr>';
     for (let j = 0; j < 24; j++) {
-      if (source[i][4][j]) {
+      if (source[i].daily[j]) {
         newrow += '<td class="yes">'+j+'</td>';
       } else {
         newrow += '<td class="no"></td>';
