0dfa52791e817f55e21e5732efd68f0aeed11aa9
Astronomy487 <astronomy487@gmail.com>
2021-01-06 10:53:40 -0600

    acnh sheet updates for donation
    also cookietest.html

--- projects/acnh/generate.js
+++ projects/acnh/generate.js
@@ -1,104 +1,146 @@
-function generate(set) {
-let source = set;
-let newrow = "";
-
-console.log("beginning generation");
-
-newrow += '<tr><th>Species Name</th>';
-if (source[0][0] == "Seaweed") {
-  newrow += '<th>Motion</th>';
-} else {
-  newrow += '<th>Location</th>';
+let donation = {
+  bug: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  fish: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  deep: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 }
-if (source[0][5] != "bug") newrow += '<th>Size</th>'
-newrow += '<th>Selling price</th>';
-console.log(shownext);
-if (shownext) {
-  newrow += '<th>Next Available</th>';
-} else {
-  newrow += '<th>Annual Availability</th><th>Daily Availability</th>';
-}
-newrow += '</tr>';
-document.getElementById("speciesbody").innerHTML = newrow;
 
-for (let i = 0; i < source.length; i++) {
-  newrow = "";
-  
-  let monthmatch;
-  if (!south) {
-    monthmatch = source[i].annual[now.getMonth()];
-  } else {
-    monthmatch = source[i].annual[(now.getMonth()+6)%12];
+function generate() {
+  let currentsetname;
+  switch (currentset) {
+    case bug:
+    currentsetname = "bug";
+      break;
+    case fish:
+    currentsetname = "fish";
+      break;
+    case deep:
+    currentsetname = "deep";
+      break;
   }
-  
-  let nextavailable;
-  if (monthmatch && source[i].daily[now.getHours()]) {
-    newrow = '<tr class="available">';
-    nextavailable = "now";
-  } else if (monthmatch) {
-    newrow = '<tr class="almostavailable">';
-    findNextAvailable:
-    for (a = now.getHours(); a < 30; a = (a+1)%24) {
-      if (source[i].daily[a]) {
-        nextavailable = hours[a];
-        break findNextAvailable;
-      }
-    }
+  let source = currentset;
+  let newrow = "";
+
+  newrow += '<tr><th></th><th>Species name</th>';
+  if (currentsetname == "deep") {
+    newrow += '<th>Motion</th>';
   } else {
-    newrow = '<tr class="unavailable">'
-    findNextAvailable:
-    for (a = now.getMonth(); a < 30; a = (a+1)%12) {
-      if (source[i].annual[a]) {
-        //next available month is found (a)
-        if (!south) {
-          nextavailable = months[a];
-        } else {
-          nextavailable = months[(a+6)%12];
-        }
-        break findNextAvailable;
-      }
-    }
+    newrow += '<th>Location</th>';
   }
-  
-  newrow += "<td>"+source[i].name+"</td>"; //name
-  newrow += "<td>"+source[i].found+"</td>"; //location
-  if (source[i][5] != "bug") newrow += "<td>"+source[i].size+"</td>"; //size, if applicable
-  newrow += "<td>"+commas(source[i].bells)+" bells</td>"; //price
-  
+  if (currentsetname != "bug") newrow += '<th>Size</th>'
+  newrow += '<th>Selling price</th>';
   if (shownext) {
-    newrow += "<td>"+nextavailable+"</td>";
+    newrow += '<th>Next available</th>';
   } else {
-    newrow += '<td class="timescell"><table class="times" id=""><tr>';
-    for (let j = 0; j < 12; j++) {
-      let availablethismonth;
-      if (!south) {
-        availablethismonth = source[i].annual[j];
-      } else {
-        availablethismonth = source[i].annual[(j+6)%12];
-      }
-      if (availablethismonth) {
-        newrow += '<td class="yes">'+monthsshort[j]+'</td>';
-      } else {
-        newrow += '<td class="no"></td>';
-      }
-    }
-    newrow += '</tr></table></td>';
-    
-    newrow += '<td class="timescell"><table class="times" id=""><tr>';
-    for (let j = 0; j < 24; j++) {
-      if (source[i].daily[j]) {
-        newrow += '<td class="yes">'+j+'</td>';
-      } else {
-        newrow += '<td class="no"></td>';
-      }
-    }
-    newrow += '</tr></table></td>';
+    newrow += '<th>Annual availability</th><th>Daily availability</th>';
   }
-  
-  newrow += "</tr>";
-  document.getElementById("speciesbody").innerHTML += newrow;
-}
+  newrow += '</tr>';
+  document.getElementById("speciesbody").innerHTML = newrow;
 
-console.log("generation complete");
-
+  for (let i = 0; i < source.length; i++) {
+    newrow = "";
+    
+    let monthmatch;
+    if (!south) {
+      monthmatch = source[i].annual[now.getMonth()];
+    } else {
+      monthmatch = source[i].annual[(now.getMonth()+6)%12];
+    }
+    
+    let nextavailable;
+    if (monthmatch && source[i].daily[now.getHours()]) {
+      newrow = '<tr class="available';
+      nextavailable = "now";
+    } else if (monthmatch) {
+      newrow = '<tr class="almostavailable';
+      findNextAvailable:
+      for (a = now.getHours(); a < 30; a = (a+1)%24) {
+        if (source[i].daily[a]) {
+          nextavailable = hours[a];
+          break findNextAvailable;
+        }
+      }
+    } else {
+      newrow = '<tr class="unavailable';
+      findNextAvailable:
+      for (a = now.getMonth(); a < 30; a = (a+1)%12) {
+        if (source[i].annual[a]) {
+          //next available month is found (a)
+          if (!south) {
+            nextavailable = months[a];
+          } else {
+            nextavailable = months[(a+6)%12];
+          }
+          break findNextAvailable;
+        }
+      }
+    }
+    
+    let donstat;
+    switch (currentsetname) {
+      case "bug": donstat = donation.bug[i]; break;
+      case "fish": donstat = donation.fish[i]; break;
+      case "deep": donstat = donation.deep[i]; break;
+    }
+    newrow += ' '+donationstages[donstat];
+    //console.log(donstat);
+    /*switch (donstat) {
+      case 0: newrow += ' uncaught'; break;
+      case 1: newrow += ' caught'; break;
+      case 2: newrow += ' donated'; break;
+    }*/
+    
+    newrow += '">';
+    
+    let donationText;
+    switch (currentsetname) {
+      case "bug":
+        donationText = donationstages[donation.bug[i]];
+        break;
+      case "fish":
+        donationText = donationstages[donation.fish[i]];
+        break;
+      case "deep":
+        donationText = donationstages[donation.deep[i]];
+        break;
+    }
+    newrow += '<td><button onclick="changeDonationStatus(\''+currentsetname+"\',"+i+')">'+donationText+'</button></td>';
+    
+    newrow += "<td>"+source[i].name+"</td>"; //name
+    newrow += "<td>"+source[i].found+"</td>"; //location
+    if (currentsetname != "bug") newrow += "<td>"+source[i].size+"</td>"; //size, if applicable
+    newrow += "<td>"+commas(source[i].bells)+" bells</td>"; //price
+    
+    if (shownext) {
+      newrow += "<td>"+nextavailable+"</td>";
+    } else {
+      newrow += '<td class="timescell"><table class="times" id=""><tr>';
+      for (let j = 0; j < 12; j++) {
+        let availablethismonth;
+        if (!south) {
+          availablethismonth = source[i].annual[j];
+        } else {
+          availablethismonth = source[i].annual[(j+6)%12];
+        }
+        if (availablethismonth) {
+          newrow += '<td class="yes">'+monthsshort[j]+'</td>';
+        } else {
+          newrow += '<td class="no"></td>';
+        }
+      }
+      newrow += '</tr></table></td>';
+      
+      newrow += '<td class="timescell"><table class="times" id=""><tr>';
+      for (let j = 0; j < 24; j++) {
+        if (source[i].daily[j]) {
+          newrow += '<td class="yes">'+j+'</td>';
+        } else {
+          newrow += '<td class="no"></td>';
+        }
+      }
+      newrow += '</tr></table></td>';
+    }
+    
+    newrow += "</tr>";
+    document.getElementById("speciesbody").innerHTML += newrow;
+  }
 }
--- projects/acnh/script.js
+++ projects/acnh/script.js
@@ -3,16 +3,19 @@ let monthsshort = ["J","F","M","A","M","J","J","A","S","O","N","D",];
 let hours = ["12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM",];
 let now = new Date();
 document.getElementById("current").innerHTML = hours[now.getHours()]+" in "+months[now.getMonth()];
+let donationstages = ["uncaught","caught","donated"];
 
 document.getElementById("menuall").click();
 let currentset;
-document.getElementById("menubugs").click();
+document.getElementById("menubug").click();
 let south;
 document.getElementById("menunorth").click();
 let shownext = true;
 document.getElementById("menunexttrue").click();
+let showbydonation = false;
+document.getElementById("menuavailable").click();
 
-generate(currentset);
+generate();
 
 function commas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -20,10 +23,12 @@ function commas(x) {
   //https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
 }
 
-function setDisplay(x) {
+function setFilter(x) {
   document.getElementById("menuall").setAttribute("class","");
   document.getElementById("menumonth").setAttribute("class","");
   document.getElementById("menucurrent").setAttribute("class","");
+  document.getElementById("menuuncaught").setAttribute("class","");
+  document.getElementById("menuundonated").setAttribute("class","");
   document.getElementById("menu"+x).setAttribute("class","active");
   
   let newcss;
@@ -37,6 +42,12 @@ function setDisplay(x) {
     case "current":
       newcss = ".unavailable, .almostavailable {display: none;}";
       break;
+    case "uncaught":
+      newcss = ".caught, .donated {display: none;}";
+      break;
+    case "undonated":
+      newcss = ".donated {display: none;}";
+      break;
   }
   document.getElementById("hiderowsstyle").innerHTML = newcss;
 }
@@ -46,16 +57,16 @@ function hemi(x) {
   document.getElementById("menusouth").setAttribute("class","");
   document.getElementById("menu"+x).setAttribute("class","active");
   south = (x == 'south');
-  generate(currentset);
+  generate();
 }
 
 function openNew(x) {
-  document.getElementById("menubugs").setAttribute("class","");
+  document.getElementById("menubug").setAttribute("class","");
   document.getElementById("menufish").setAttribute("class","");
   document.getElementById("menudeep").setAttribute("class","");
   document.getElementById("menu"+x).setAttribute("class","active");
   switch (x) {
-    case "bugs":
+    case "bug":
       currentset = bug;
       break;
     case "fish":
@@ -73,5 +84,43 @@ function showNext(x) {
   document.getElementById("menunextfalse").setAttribute("class","");
   document.getElementById("menunext"+x.toString()).setAttribute("class","active");
   shownext = x;
-  generate(currentset);
+  generate();
+}
+
+function changeDonationStatus(setname,index) {
+  switch (setname) {
+    case "bug":
+      donation.bug[index] = (donation.bug[index]+1)%3;
+      break;
+    case "fish":
+      donation.fish[index] = (donation.fish[index]+1)%3;
+      break;
+    case "deep":
+      donation.deep[index] = (donation.deep[index]+1)%3;
+      break;
+  }
+  generate();
+  //document.getElementById(setname+index.toString()).innerHTML = "test";
+}
+
+function setColor(mode) {
+  document.getElementById("menuavailable").setAttribute("class","");
+  document.getElementById("menudonation").setAttribute("class","");
+  document.getElementById("menu"+mode).setAttribute("class","active");
+  if (mode == "available") {
+    showbydonation = false;
+  } else {
+    showbydonation = true;
+  }
+  let newcss;
+  switch (showbydonation) {
+    case false:
+      newcss = ".available {background-color: var(--green);} .almostavailable {background-color: var(--yellow);} .unavailable {background-color: var(--red);}";
+      break;
+    case true:
+      newcss = ".uncaught {background-color: var(--red);} .caught {background-color: var(--yellow);} .donated {background-color: var(--green);}";
+      break;
+  }
+  document.getElementById("setcolorstyle").innerHTML = newcss;
+  generate();
 }
